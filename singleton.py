ae730f93fd4b3f62404b81da5ccd944e1bddbde6
Vasyl Voityk <vasyl.voityk@globallogic.com>
2018-03-14 17:56:20 +0200

    singleton stuff

--- singleton.py
+++ singleton.py
@@ -1,4 +1,9 @@
-class Singleton:
+"""
+Singleton stuff.
+"""
+
+
+class SingletonOne:
     __instance = None
 
     def __new__(cls, *args, **kwargs):
@@ -6,6 +11,21 @@ class Singleton:
             cls.__instance = super().__new__(cls, *args, **kwargs)
 
         return cls.__instance
+
+
+class SingletonMeta(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(SingletonMeta, cls).__call__(*args,
+                                                                     **kwargs)
+
+        return cls._instances[cls]
+
+
+class SingletonTwo(metaclass=SingletonMeta):
+    pass
 
 
 class Borg:
